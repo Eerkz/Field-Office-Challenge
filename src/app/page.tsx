f0a10c2cab8b5b2454fdc1fc6f9cf610fb5ff399
Naarkz <wcalemios@gmail.com>
2024-07-27 17:44:25 +0800

    feat: Add header texts animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,35 @@
+"use client"
+
+import { motion } from "framer-motion";
 import Image from "next/image";
+
+const commonInitial = { opacity: 0, transform: "translateY(-25px)" };
+const commonAnimate = { opacity: 1, transform: "translateY(0)" };
+const commonTransition = { ease: "ease" };
+
+const joinUsAnimation = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  transition: { ...commonTransition, delay: 1.2, duration: 1 },
+};
+
+const h1Animation = {
+  initial: commonInitial,
+  animate: commonAnimate,
+  transition: { ...commonTransition, delay: 0, duration: 0.6 },
+};
+
+const pAnimation = {
+  initial: commonInitial,
+  animate: commonAnimate,
+  transition: { ...commonTransition, delay: 0.3, duration: 0.8 },
+};
+
+const buttonAnimation = {
+  initial: commonInitial,
+  animate: commonAnimate,
+  transition: { ...commonTransition, delay: 0.7, duration: 0.9 },
+};
 
 export default function Home() {
   return (
@@ -6,30 +37,50 @@ export default function Home() {
       <header className="ease-in-out sticky top-0 z-10 dark:bg-secondary min-[993px]:h-[71px] h-[64px]"></header>
       <main className="w-full flex items-center justify-center text-fo-black min-[425px]:px-16 px-4 pb-[96px]">
         <section className="container flex flex-col items-center pt-10 lg:pt-40">
-          <div className="flex items-center gap-[12px] font-medium p-[4px_8px_4px_4px] text-sm text-secondary bg-[#EEEEEE] rounded-[8px] hover:cursor-pointer border border-[#6B9FFF] border-opacity-0 hover:border-opacity-100 active:border-opacity-100 outline-none ring-0 ring-[#6B9FFF] hover:ring-[3px] focus:ring-[3px] active:ring-[1px] ring-opacity-0 hover:ring-opacity-30 focus:ring-opacity-30 active:ring-opacity-30 transition-all duration-500 ease-out">
+          <motion.div
+            initial={joinUsAnimation.initial}
+            animate={joinUsAnimation.animate}
+            transition={joinUsAnimation.transition}
+            className="flex items-center gap-[12px] font-medium p-[4px_8px_4px_4px] text-sm text-secondary bg-[#EEEEEE] rounded-[8px] hover:cursor-pointer border border-[#6B9FFF] border-opacity-0 hover:border-opacity-100 active:border-opacity-100 outline-none ring-0 ring-[#6B9FFF] hover:ring-[3px] focus:ring-[3px] active:ring-[1px] ring-opacity-0 hover:ring-opacity-30 focus:ring-opacity-30 active:ring-opacity-30 transition-all duration-500 ease-out"
+          >
             <span className="p-[2px_10px] align-middle bg-white rounded-[8px] text-fo-blue border-fo-blue border-[1px] text-sm">
               Join us
             </span>
             <div className="flex items-center gap-[4px]">
               <span>✨</span> Come join us at Convention
             </div>
-          </div>
+          </motion.div>
           <div className="my-6 text-center">
-            <h1 className=" [font-kerning:none] duration-700">
+            <motion.h1
+              initial={h1Animation.initial}
+              animate={h1Animation.animate}
+              transition={h1Animation.transition}
+              className="[font-kerning:none]"
+            >
               The ultimate influencer <br /> discovery platform.
-            </h1>
-            <p className="fo-d-body mt-6">
+            </motion.h1>
+            <motion.p
+              initial={pAnimation.initial}
+              animate={pAnimation.animate}
+              transition={pAnimation.transition}
+              className="fo-d-body mt-6"
+            >
               So good, you&apos;ll keep it forever.
-            </p>
+            </motion.p>
           </div>
-          <button className="bg-fo-black fo-button-lg mt-4 text-white border-fo-black border-[1px] border-solid fo-button-primary hover:border-[#505967]">
+          <motion.button
+            initial={buttonAnimation.initial}
+            animate={buttonAnimation.animate}
+            transition={buttonAnimation.transition}
+            className="bg-fo-black fo-button-lg mt-4 text-white border-fo-black border-[1px] border-solid fo-button-primary hover:border-[#505967]"
+          >
             Book a demo
-          </button>
+          </motion.button>
 
           <div className="flex justify-center w-full relative">
             <div className="relative sm:aspect-[1.87] aspect-[1.3149] sm:w-[82.5%] w-full mt-[80px] flex justify-center items-center">
               <div className="absolute z-[-1] top-[-105px] w-screen h-[105%] sm:block hidden">
-                <div className="w-full h-[125%] dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-[#E7E5E4]/[0.65]"></div>
+                <div className="w-full h-[125%] dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-[#E7E5E4]/[0.65]"></div>
               </div>
               <Image
                 src="/thumbnail.png"
@@ -46,8 +97,8 @@ export default function Home() {
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:-translate-y-[65%] lg:scale-[1.2]">
                 <div className="absolute inset-0 animate-[video-hero-ripple-outer_4s_ease-out_infinite] rounded-full border border-white-100 bg-[radial-gradient(#266df0_27.08%,#266df0_100%)]"></div>
                 <div className="absolute inset-0 animate-[video-hero-ripple-inner_4s_ease-out_infinite] rounded-full border border-white-100 bg-[radial-gradient(#266df0_27.08%,#266df0_100%)]"></div>
-                <button className="relative p-[21px] transition-all bg-[#E0E0E0] animate-[video-hero-pulse_4s_ease-out_infinite] rounded-full sm:w-[84px] sm:h-[84px] w-[64px] h-[64px]  flex items-center justify-center ring-0 ring-blue-500 ring-offset-1 ring-offset-white-100">
-                  <div className="w-0 h-0 border-t-transparent border-l-[22.86px] border-b-[11.43px] border-t-[11.43px] border-l-fo-black  ml-[6px] border-b-transparent"></div>
+                <button className="relative p-[21px] transition-all bg-[#E0E0E0] animate-[video-hero-pulse_4s_ease-out_infinite] rounded-full sm:w-[84px] sm:h-[84px] w-[64px] h-[64px] flex items-center justify-center ring-0 ring-blue-500 ring-offset-1 ring-offset-white-100">
+                  <div className="w-0 h-0 border-t-transparent border-l-[22.86px] border-b-[11.43px] border-t-[11.43px] border-l-fo-black ml-[6px] border-b-transparent"></div>
                 </button>
               </div>
             </div>
