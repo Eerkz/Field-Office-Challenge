ec5d4c07f285cbdf269a87110e67e65a6e07625b
Naarkz <wcalemios@gmail.com>
2024-07-28 16:15:23 +0800

    refactor: Change shimmer color

--- src/components/ui/AnimatedShinyText.tsx
+++ src/components/ui/AnimatedShinyText.tsx
@@ -23,8 +23,7 @@ const AnimatedShinyText: FC<AnimatedShinyTextProps> = ({
       className={cn(
         "mx-auto max-w-md text-neutral-600/70",
         "animate-shimmer bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shimmer-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]",
-        "bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent  dark:via-white/80",
-
+        "bg-gradient-to-r from-transparent via-fo-black via-50% to-transparent",
         className
       )}
     >
