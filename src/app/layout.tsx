d7d58d663987d3724a0c33670915bccc027d0246
Naarkz <wcalemios@gmail.com>
2024-07-28 16:43:01 +0800

    feat: Add other favicon variants

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Favicon from "/public/favicon.ico";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600"] });
@@ -8,7 +7,38 @@ const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600"] });
 export const metadata: Metadata = {
   title: "Streamforge",
   description: "The ultimate influencer discovery platform.",
-  icons: [{ rel: "icon", url: Favicon.src }],
+  icons: [
+    { rel: "icon", url: "/favicons/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/favicons/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      url: "/android-chrome-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "512x512",
+      url: "/android-chrome-512x512.png",
+    },
+  ],
 };
 
 export default function RootLayout({
